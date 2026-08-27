8c7eb4e66f7d16610ab338a823be079ff2dc46c6
TheGH05T141 <anguyen141@huskers.unl.edu>
2026-08-27 13:18:40 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Andy Nguyen
+ * Date: 2026/08/27
+ *
+ * A simple hello world program in C and prints major.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("I love the School of Computing!\n");
+  printf("Major: Computer Engineer\n");
+  return 0;
+}
